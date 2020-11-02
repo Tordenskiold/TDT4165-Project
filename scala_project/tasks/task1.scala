afd9f7a3c6a44017f095f2fa65f31ec375ef043d
ivhak <hakonseniver@yahoo.no>
2020-11-02 08:56:53 +0100

    Partition task1.scala into task1 and task2.
    Move the tasks in task 2 into it's own file.

--- scala_project/tasks/task1.scala
+++ scala_project/tasks/task1.scala
@@ -1,4 +1,3 @@
-
 object Hello extends App {
 
 	// Task 1a
@@ -37,51 +36,6 @@ object Hello extends App {
 		else
 			return fibonacci(n - 1, b, a + b)
 	}
-
-
-	// Task 2a
-	def embedInThread(inputFunction: () => Unit): Thread = {
-		return new Thread(new Runnable {
-			def run() {
-				inputFunction()
-			}
-		})
-	}
-
-
-	// Task 2bc
-	private var counter: Int = 0
-	def increaseCounter(): Unit = {
-		this.synchronized {
-			counter += 1
-		}
-	}
-
-	def printCounter(): Unit = {
-		println(counter)
-	}
-
-
-	// Task 2d
-	/*
-	A deadlock is used to describe a situation where a program cannot proceed because parts of the
-	program have circular dependencies, meaning a part of the program needs the result from another
-	part of the program, which in turn needs the result from another part of the program, and at
-	some point in this chain of dependencies, the first part of the program mentioned is a
-	dependency. The phenomenon we just described is called a circular dependency. Avoiding circular
-	dependencies is key to avoiding deadlocks.
-	The code below causes a deadlock if either ObjectA.valueA or ObjectB.valueB is called. This is
-	because calling ObjectA.valueA will result in a call to ObjectB.valueB, which in turn will
-	result in a call to ObjectA.valueA, and we're back to the starting point, in a deadlock.
-	*/
-	object ObjectA {
-		lazy val valueA: Int = ObjectB.valueB
-	}
-	object ObjectB {
-		lazy val valueB: Int = ObjectA.valueA
-	}
-
-
 }
 
 
